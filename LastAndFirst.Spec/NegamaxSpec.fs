593b00dafe780f806e9de3d6acdaf1e60271d4af
Karak <karak97@gmail.com>
2010-08-20 21:45:28 +0900

    implement Nega-max best way finding

---
+++ LastAndFirst.Spec/NegamaxSpec.fs
@@ -0,0 +1,42 @@
+﻿#light
+module LastAndFirst.Spec.NegamaxSpec
+
+open LastAndFirst
+open LastAndFirst.Negamax
+open NaturalSpec
+
+let leaf = Node("0", [])
+
+[<Scenario>]
+let ``When it is leaf, defeat at 1st``()  =
+  Given leaf
+    |> When calculating negamax
+    |> It should equal (Defeat, ["0"])
+    |> Verify
+
+let whoHasLeafOnly = Node ("1", [leaf])
+
+[<Scenario>]
+let ``When its child is a leaf, victory at 2nd``()  =
+  Given whoHasLeafOnly
+    |> When calculating negamax
+    |> It should equal (Victory, ["1" ; "0"])
+    |> Verify
+
+let whoHasLeaf = Node ("1", [leaf ; whoHasLeafOnly ; whoHasLeafOnly])
+
+[<Scenario>]
+let ``When any of its children is a leaf, victory at 2nd``()  =
+  Given whoHasLeaf
+    |> When calculating negamax
+    |> It should equal (Victory, ["1" ; "0"])
+    |> Verify
+
+let whoseNextHasLeafOnly = Node ("2", [whoHasLeafOnly ; whoHasLeafOnly ; whoHasLeaf])
+
+[<Scenario>]
+let ``When any of its children has a leaf, defeat at 3rd``()  =
+  Given whoseNextHasLeafOnly
+    |> When calculating negamax
+    |> It should equal (Defeat, ["2" ; "1" ; "0"])
+    |> Verify
---
+++ LastAndFirst/Negamax.fs
@@ -0,0 +1,28 @@
+﻿#light
+module LastAndFirst.Negamax
+
+type Score =  Victory | Defeat
+let reverse score =
+   match score with
+    | Victory -> Defeat
+    | Defeat -> Victory
+
+///TODO: 巨大な木に対応するためには、遅延評価を実現できる必要がある
+type Tree = Node of string * Tree list
+
+///最善の評価値とその際の履歴を求める
+let rec negamax (tree: Tree) : Score * string list =
+   match tree with
+   | Node (myAnswer, children) ->
+     let length = children.Length
+     if length = 0 then
+       (Defeat, [myAnswer])
+     else
+       let next = children.Head
+       let (nextScore, nextHistory) = negamax next
+       if nextScore = Defeat then
+         (Victory, myAnswer :: nextHistory)
+       else if length = 1 then
+         ((reverse nextScore), myAnswer :: nextHistory)
+       else  //本当に子どもがいない場合と子どもの中にDefeatがない場合を区別する必要がある
+         negamax (Node(myAnswer, children.Tail))
